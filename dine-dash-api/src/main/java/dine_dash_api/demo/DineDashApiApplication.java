2d415e38f6b6187b9afeae19afa177d87959b40e
muraddevs <murad.aghamirzayevv@gmail.com>
2024-11-01 11:45:11 +0400

    first commit

---
+++ dine-dash-api/src/main/java/dine_dash_api/demo/DineDashApiApplication.java
@@ -0,0 +1,13 @@
+package dine_dash_api.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineDashApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineDashApiApplication.class, args);
+	}
+
+}
---
+++ dine-dash-api/src/test/java/dine_dash_api/demo/DineDashApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dine_dash_api.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineDashApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
